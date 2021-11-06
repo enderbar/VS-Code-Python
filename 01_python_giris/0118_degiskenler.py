c99d4f720f31bad09bc3f7a5d39456cd306c4f29
enderbar <enderbar123@hotmail.com>
2021-11-06 13:58:57 +0300

    06112021-1358

---
+++ 01_python_giris/0118_degiskenler.py
@@ -0,0 +1,6 @@
+name = "Jhon"
+print(name)
+Print = "Jhon"
+print(Print)
+print = "print"
+print(34)
---
+++ 01_python_giris/0119_concatation.py
@@ -0,0 +1,10 @@
+okulNo = "3169"
+ad = "Baya"
+soyad = "Komigim Ha"
+
+print(" bunları bas " + ad + " sonra bunları bas " + soyad + " şimdi de bunu bas " + okulNo)
+
+ad = "Barış"
+kahve = "Filtre Kahve"
+
+print(" Merhaba ben " + ad + " en sevdiğim kahve " + kahve)
