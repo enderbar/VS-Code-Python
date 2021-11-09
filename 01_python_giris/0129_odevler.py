0827d649c8d46d6cef83bfa6a492e614476a73f7
enderbar <enderbar123@hotmail.com>
2021-11-09 15:23:43 +0300

    09112021-1523

--- 01_python_giris/0129_odevler.py
+++ 01_python_giris/0129_odevler.py
@@ -25,6 +25,39 @@ sonuc=(x**2+y**2)**0.5
 print(f"Girilen noktalar arası uzaklık: {round(sonuc, 2)}")
 """
 #endregion
+#region ödev 4
+"""
+at=int(input("Aylık tüketim giriniz:"))
+aeb=at*0.39736
+db=aeb*0.2474
+teb=aeb+db
+kdv=(teb+3.97+1.39+9.92)*0.18
+vgt=3.97+1.39+9.92+kdv
+topFatura=vgt+teb
+print(f"Aylık elektrik faturanız: {topFatura} TL")
+"""
+#endregion
+#region ödev 5
+"""
+gbi=float(input("Güncel benzin indeksini giriniz:"))
+yt=float(input("100 km başına yakıt tüketimini giriniz:"))
+alınanYol=int(input("Kaç kilometre yol aldığınızı giriniz:"))
+yakit=alınanYol/100*yt*gbi
+print(f"Toplam Yakıt Tüketiminiz: {yakit} Litre")
+"""
+#endregion
+#region ödev 6
+"""
+x=input("Ürün adınızı giriniz:")
+y=int(input(f"{x} - Satın alınacak adet:"))
+z=float(input("Ürün fiyatı giriniz:"))
+t=int(input("% İndirim oranını giriniz:"))
+tutar=z*y*((100-t)/100)
+print(f"Ödenecek tutar: {tutar} TL")
+"""
+#endregion
+
+
 
 
 
