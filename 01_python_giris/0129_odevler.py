871ec5ecb7b92d902ffd8be7ec59b7a13ad63fd8
enderbar <enderbar123@hotmail.com>
2021-11-09 11:43:15 +0300

    09112021-1143

---
+++ 01_python_giris/0129_odevler.py
@@ -0,0 +1,38 @@
+#region ödev 1
+"""
+km = int(input("Kilomentre bilgisi giriniz: "))
+mil = km/1.609344
+print(f" Girilen kilometre bilgisi: {km} \n Girilen değerin mil karşılığı: {mil} ")
+"""
+#endregion
+#region ödev 2
+"""
+a = int(input("A köşe açısını giriniz: "))
+b = int(input("B köşe açısını giriniz: "))
+c = 180-a-b 
+print(f"Girilen A değeri: {a}\nGirilen B değeri: {b}\nC köşe açısı: {c}")
+"""
+#endregion
+#region ödev 3
+"""
+x1 = int(input("x1 değeri giriniz:"))
+x2 = int(input("x2 değeri giriniz:"))
+y1 = int(input("y1 değeri giriniz:"))
+y2 = int(input("y2 değeri giriniz:"))
+x=abs(x1-x2)
+y=abs(y1-y2)
+sonuc=(x**2+y**2)**0.5
+print(f"Girilen noktalar arası uzaklık: {round(sonuc, 2)}")
+"""
+#endregion
+
+
+
+
+
+
+
+
+
+
+
